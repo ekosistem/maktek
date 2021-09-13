ea4f15d96885dcf27ff3e903c3fcc365553efc2b
ekosistem <soyozekrem@hotmail.com>
2021-09-13 09:04:38 +0300

    login ve db bağlantısı yapıldı

---
+++ maktek/Controllers/LoginController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using maktek.Models;
+
+namespace maktek.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+
+        aksateknikEntities db = new aksateknikEntities();
+        public ActionResult Index()
+        {
+            var model = db.kullanici.ToList();
+            return View();
+        }
+    }
+}
